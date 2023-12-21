739b0890f99e879079696fe57e70d36b6291703f
masudo40 <masudinfo128@gamil.com>
2023-12-21 09:08:58 +0600

    set up environment and test

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div
+      className={`bg-[url(https://i.ibb.co/kcmP5Jd/banner.jpg)] bg-cover bg-center bg-no-repeat h-44`}
+    ></div>
+  );
+};
+
+export default Home;
---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+export default router;
